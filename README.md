f2b6903225a4abc91be902d4e8e578fdad883c50
Ben Jefferies <benjefferies@echosoft.uk>
2018-01-28 19:45:09 +0000

    Fix filtering

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Raiblock payments
 A simple app to notify (print to console) of any payments incoming payments
+
+## Raiblocks node
+Setup raiblocks node by following https://1xrb.com/support-the-network/
+
+## Setup
+```bash
+pipenv install
+python run.py "[::1]" "xrb_youraddress"
+```
+
+The output looks something like
+
+```
+New transaction from xrb_youraddress for 400000000000000000000000000
+New transaction from xrb_youraddress for 200000000000000000000000000
+New transaction from xrb_youraddress for 300000000000000000000000000
+New transaction from xrb_youraddress for 1000000000000000000000000000
+New transaction from xrb_youraddress for 100000000000000000000000000
+```
--- run.py
+++ run.py
@@ -2,9 +2,10 @@ import sys
 
 import requests
 import time
+import json
 
-host = sys.argv[0]
-account = sys.argv[1]
+host = sys.argv[1]
+account = sys.argv[2]
 trans = []
 while True:
     data = {
@@ -12,10 +13,11 @@ while True:
         'account': account,
         'count': 10,
     }
-    data = requests.post(f'{host}:7076', data).json()
-    new_trans = set(trans) - set(data.history)
+    resp = requests.post(f'http://{host}:7076', json.dumps(data)).json()
+    new_trans = [x for x in resp['history'] if x not in trans]
     for tran in new_trans:
-        if tran.type == 'receive':
-            print(f'New transaction from {tran.account} for {tran.amount}')
-
+        if tran['type'] == 'receive':
+            print('New transaction from {} for {}'.format(tran['account'], tran['amount']))
+    trans = resp.get('history', [])
     time.sleep(5)
+
