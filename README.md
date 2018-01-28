b494343cf8fb5f309b3b5a5e8e7c5b7d9df62eef
Ben Jefferies <benjefferies@echosoft.uk>
2018-01-28 18:39:40 +0000

    App to notify of payments

---
+++ README.md
@@ -0,0 +1,2 @@
+# Raiblock payments
+A simple app to notify (print to console) of any payments incoming payments
---
+++ run.py
@@ -0,0 +1,18 @@
+import sys
+
+import requests
+
+host = sys.argv[0]
+account = sys.argv[1]
+trans = []
+while True:
+    data = {
+        'action': 'account_history',
+        'account': account,
+        'count': 10,
+    }
+    data = requests.post(f'{host}:7076', data).json()
+    new_trans = set(trans) - set(data.history)
+    for tran in new_trans:
+        if tran.type == 'receive':
+            print(f'New transaction from {tran.account} for {tran.amount}')
