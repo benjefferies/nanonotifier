1ac7b79e64983bbf2b749d44f46f621a4c9024d0
Ben Jefferies <benjefferies@echosoft.uk>
2018-01-31 15:09:43 +0000

    Set source email

--- ses.py
+++ ses.py
@@ -3,6 +3,7 @@ import os
 from botocore.exceptions import ClientError
 
 
+source_email = os.getenv('SES_SOURCE_EMAIL', 'raiblockpayments@echosoft.uk')
 AWS_REGION = os.getenv('AWS_REGION', 'us-west-2')
 
 # Create a new SES resource and specify a region.
@@ -10,7 +11,6 @@ client = boto3.client('ses', region_name=AWS_REGION)
 
 
 def send(email_user, subject, message):
-    email_domain = email_user.split("@")[1]
     # Try to send the email.
     try:
         # Provide the contents of the email.
@@ -23,16 +23,16 @@ def send(email_user, subject, message):
             Message={
                 'Body': {
                     'Text': {
-                        '"UTF-8"': "UTF-8",
+                        'Charset': "UTF-8",
                         'Data': message,
                     },
                 },
                 'Subject': {
-                    '"UTF-8"': "UTF-8",
+                    'Charset': "UTF-8",
                     'Data': subject,
                 },
             },
-            Source=f'raiblockpayments@{email_domain}',
+            Source='raiblockpayments@echosoft.uk',
         )
     # Display an error if something goes wrong.
     except ClientError as e:
