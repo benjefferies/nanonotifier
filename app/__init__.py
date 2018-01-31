7213187b89a4754cdf4e8c8bc24721989928e069
Ben Jefferies <benjefferies@echosoft.uk>
2018-01-31 21:49:27 +0000

    Remove formatting

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,10 @@ import logging
 
 import sys
 
-FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-logging.basicConfig(format=FORMAT)
+logging.basicConfig()
 root = logging.getLogger()
 root.setLevel(logging.INFO)
 
 ch = logging.StreamHandler(sys.stdout)
 ch.setLevel(logging.INFO)
-ch.setFormatter(FORMAT)
 root.addHandler(ch)
