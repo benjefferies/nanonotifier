84ba7e237d07199b978225fa56c04313fa4a0dfb
Ben Jefferies <benjefferies@echosoft.uk>
2018-01-31 21:38:46 +0000

    Add repr

--- app/models.py
+++ app/models.py
@@ -13,5 +13,8 @@ class Notification(Base):
     email = Column(String, primary_key=True)
     account = Column(String)
 
+    def __repr__(self):
+        return f"<Notification(email='{self.email}', account='{self.account}')>"
+
 
 Base.metadata.create_all(engine)
