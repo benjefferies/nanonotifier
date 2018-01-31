f6692a01e0b475e1c4fbedb2f01a2a8022635e34
Ben Jefferies <benjefferies@echosoft.uk>
2018-01-31 07:09:48 +0000

    Send email notifications

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
+import smtplib
 import sys
 
+import os
 import requests
 import time
 import json
 
 
-host = sys.argv[1]
-account = sys.argv[2]
+account = sys.argv[1]
+email_user = sys.argv[2]
+email_password = sys.argv[3]
 data = {
     'action': 'account_history',
     'account': account,
@@ -15,28 +18,43 @@ data = {
 
 
 def get_trans_history():
+    host = os.getenv('RAIBLOCKS_HOST', '[::1]')
     return requests.post(f'http://{host}:7076', json.dumps(data)).json().get('history', [])
 
 
-last_known_trans = get_trans_history()
-while True:
+def notify(total, message):
+    subject = 'Received {:10.5f} XRB'.format(total)
+    if os.getenv('SMTP_ENABLED', False):
+        server = smtplib.SMTP(os.getenv('SMTP_HOST'), os.getenv('SMTP_PORT'))
+        server.login(email_user, email_password)
+        domain = email_user.split("@")[1]
+        email = f'Subject: {subject}\n\n{message}'
+        server.sendmail(f'raiblockpayments@{domain}', email_user, email)
+    else:
+        print(subject)
+        print(message)
+
+
+def check_account():
     trans_history = get_trans_history()
     new_trans = [x for x in trans_history if x not in last_known_trans]
     total = 0
     message = ''
     for tran in new_trans:
         if tran['type'] == 'receive':
-            account = tran['account']
+            from_account = tran['account']
             amount = float(tran['amount'])
             if amount != 0:
                 amount /= 1.0e+30
             total += amount
-            message += f'New transaction from {account} for ' + "{:10.5f}\n".format(amount)
-
+            message += f'New transaction from {from_account} for ' + "{:10.5f}\n".format(amount)
     if total > 0:
-        subject = 'Received {:10.5f} XRB'.format(total)
-        print(subject)
-        print(message)
-    last_known_trans = trans_history
+        notify(total, message)
     time.sleep(5)
+    return trans_history
 
+
+if __name__ == '__main__':
+    last_known_trans = get_trans_history()
+    while True:
+        last_known_trans = check_account()
