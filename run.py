f6ecb1d532ed9e9512479188d758afe1d2fd6f29
Ben Jefferies <benjefferies@echosoft.uk>
2018-01-31 22:06:28 +0000

    Remove emails

--- run.py
+++ run.py
@@ -19,18 +19,7 @@ def notifications():
 
     notification = Notification()
     notification.account = 'xrb_3xjnmhz5oc1p6oabo15a33nuu86uwwx87f3qot36eax73ous6ez9ytdoyrcr'
-    notification.email = 'jack.h.jefferies@gmail.com'
-    session.add(notification)
-
-    notification = Notification()
-    notification.account = 'xrb_3xjnmhz5oc1p6oabo15a33nuu86uwwx87f3qot36eax73ous6ez9ytdoyrcr'
     notification.email = 'benjefferies@echosoft.uk'
-    session.add(notification)
-    session.commit()
-
-    notification = Notification()
-    notification.account = 'xrb_3xjnmhz5oc1p6oabo15a33nuu86uwwx87f3qot36eax73ous6ez9ytdoyrcr'
-    notification.email = 'ljefferies98@gmail.com'
     session.add(notification)
     session.commit()
 
