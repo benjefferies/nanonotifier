3a372da6a91979105259bab8e0511ffd37f6e3b5
Ben Jefferies <benjefferies@echosoft.uk>
2018-02-02 06:54:36 +0000

    Always check for new accounts

--- run.py
+++ run.py
@@ -1,30 +1,27 @@
 import logging
+import os
 import time
 from collections import defaultdict
 
-import os
-
 from app.models import Subscription, session
-from app.nano import get_trans_history, check_account
+from app.nano import check_account
 
 logger = logging.getLogger(__name__)
 
 
 if __name__ == '__main__':
-    subToEmails = defaultdict(list)
-    logger.info('Loading subscriptions')
-    for subscription in session.query(Subscription):
-        subToEmails[subscription.account].append(subscription.email)
-
-    last_known_trans = {}
-    for account in subToEmails.keys():
-        logger.info(f'Getting transaction history for {account}')
-        last_known_trans[account] = get_trans_history(account)
-
+    last_known_trans = defaultdict(list)
     while True:
-        for account in last_known_trans.keys():
+        subToEmails = defaultdict(list)
+        logger.info('Loading email subscriptions')
+
+        # Map account to emails
+        for subscription in session.query(Subscription):
+            subToEmails[subscription.account].append(subscription.email)
+
+        for account in subToEmails.keys():
             logger.info(f'Checking for new transactions for {account}')
-            account_last_known_trans = last_known_trans[account]
             emails = subToEmails[account]
+            account_last_known_trans = last_known_trans[account]
             last_known_trans[account] = check_account(account, account_last_known_trans, emails)
-        time.sleep(int(os.getenv('TIMEOUT', 5)))
+        time.sleep(int(os.getenv('TIMEOUT', 60)))
