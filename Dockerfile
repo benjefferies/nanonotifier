2877b38a0de28fdf1a7295a2474dfaf27311475e
Ben Jefferies <benjefferies@echosoft.uk>
2018-02-16 07:33:54 +0000

    Delete run.sh

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ WORKDIR /app
 COPY . /app
 RUN pip3 install pipenv==8.3.1 && pipenv install --deploy --system
 
-CMD ["/app/run.sh"]
+CMD ["python", "run.py"]
--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@ HOST = os.getenv('RAIBLOCKS_HOST', '[::1]')
 EMAIL_ENABLED = os.getenv('EMAIL_ENABLED', 'False') == 'True'
 AWS_REGION = os.getenv('AWS_REGION', 'us-west-2')
 TIMEOUT = int(os.getenv('TIMEOUT', 60))
-DB_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL')
