bbfbccb93de7c396a8f87d327edacbcdca62ffc6
Ben Jefferies <benjefferies@echosoft.uk>
2018-02-17 07:12:32 +0000

    Recover from request errors (#13)

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
-HOST = os.getenv('RAIBLOCKS_HOST', '127.0.0.1')
+HOST = os.getenv('RAIBLOCKS_HOST', '[::1]')
 EMAIL_ENABLED = os.getenv('EMAIL_ENABLED', 'False') == 'True'
 AWS_REGION = os.getenv('AWS_REGION', 'us-west-2')
 TIMEOUT = int(os.getenv('TIMEOUT', 60))
