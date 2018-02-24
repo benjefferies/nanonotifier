4ea56856b7ce6315951ec6a327334671be3d9967
Ali1 <ali.m.bakir@gmail.com>
2018-02-24 23:10:48 +0000

    Update run.py

--- run.py
+++ run.py
@@ -18,16 +18,19 @@ if __name__ == '__main__':
         last_known_pending[subscription.account] = get_pendings(subscription.account)
     while True:
         subToEmails = defaultdict(list)
+        subToHttps = defaultdict(list)
         logger.info('Loading email subscriptions')
 
-        # Map account to emails
+        # Map account to emails and HTTP addresses
         for subscription in session.query(Subscription):
             subToEmails[subscription.account].append(subscription.email)
+            subToHttps[subscription.account].append(subscription.http)
 
         for account in subToEmails.keys():
             logger.info(f'Checking for new transactions for {account}')
             emails = subToEmails[account]
+            https = subToEmails[http]
             account_last_known_trans = last_known_trans[account]
-            last_known_trans[account] = check_account_for_new_transactions(account, account_last_known_trans, emails)
-            last_known_pending[account] = check_account_for_new_pending(account, last_known_pending[account], emails)
+            last_known_trans[account] = check_account_for_new_transactions(account, account_last_known_trans, emails, https)
+            last_known_pending[account] = check_account_for_new_pending(account, last_known_pending[account], emails, https)
         time.sleep(TIMEOUT)
